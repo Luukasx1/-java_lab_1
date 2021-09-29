a2bbdee75727b3a0b1bfb0cd5bb5b3ded819797f
Luukasx1 <88154254+Luukasx1@users.noreply.github.com>
2021-09-29 15:19:08 +0200

    Delete Main.java

--- src/Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-
-    public static void main(String[] args) {
-//        ćw.1 napisać po kilka przykładów wypisywania tekstu w konsoli,
-//        Użyć każdej z opcji
-System.out.println("gesgs");
-
-    }
-}
