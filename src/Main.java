68201cafd299debd15d2ffcf397515e3f38e2367
uzytkownik <uzytkownik@kursGIT.pl>
2021-10-17 15:23:37 +0200

    Rozwiązane ćw 1 z Lab1

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,12 @@ public class Main {
 //        ćw.1 napisać po kilka przykładów wypisywania tekstu w konsoli,
 //        Użyć każdej z opcji
 
+        System.out.println("hello world");
+        System.out.println("Napis w Javie");
+        System.out.printf("Wynikiem dzialania %s jest  %d.\n", "4+1", 5);
+        System.out.print("Wczoraj caly");
+        System.out.print(" dzien");
+        System.out.print(" gralem w Diablo");
 
     }
 }
