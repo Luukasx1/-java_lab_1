ff8156ba0b479b3cb3a2597e0a2f679eac5f637c
uzytkownik <uzytkownik@kursGIT.pl>
2021-10-29 15:22:35 +0200

    Rozwiązane ćw 6 z Lab1

--- src/Main.java
+++ src/Main.java
@@ -8,6 +8,15 @@ public class Main {
 
         /* zad1. stworzyć tablicę typu double 5-cio elementową,
         przypisać do niej wartości i wyświetlić */
+        double[] tab1 = new double[5];
+        tab1[0] = 1;
+        tab1[1] = 2;
+        tab1[2] = 3;
+        tab1[3] = 4;
+        tab1[4] = 5;
+
+        System.out.println(tab1.length);
+
 
     }
 }
